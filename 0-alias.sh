f7e8e0af94f9e500403ff1f5c1affbd9219cd4b5
Andrew Grimberg <agrimberg@linuxfoundation.org>
2024-04-02 11:31:24 -0700

    Fix: Force remote terminals to be xterm
    xterm setup has better color defaults than the default xterm-256color
    sets.
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- 0-alias.sh
+++ 0-alias.sh
@@ -9,5 +9,9 @@ alias jenkins-jobs='/home/tykeal/venv/jjb/bin/jenkins-jobs'
 # Jenkins Job Builder
 alias jjb='jenkins-jobs --conf /home/tykeal/jjb/jenkins-jobs.conf'
 
+# make sure that remote terminals work as I want them too (proper colors)
+# some systems have bad colors for 256 color terminals so force xterm
+alias ssh='TERM=xterm /usr/bin/ssh'
+
 
 alias scorecard='podman run -e GITHUB_AUTH_TOKEN=$(gh auth token) gcr.io/openssf/scorecard:stable --show-details'
